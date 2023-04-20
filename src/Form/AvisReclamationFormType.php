9859ceba92959b834751baae8708e993b73ff2c7
feriel-sahbi <feriel.sahbi@esprit.tn>
2023-04-20 22:51:53 +0100

    avis updated

--- src/Form/AvisReclamationFormType.php
+++ src/Form/AvisReclamationFormType.php
@@ -5,13 +5,27 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
+use Symfony\Component\Form\FormFactoryInterface; 
+use Symfony\Component\Validator\Context\ExecutionContextInterface; // Importer la classe ExecutionContextInterface
+use Symfony\Component\Validator\Constraints\Callback;
+
+
+
 class AvisReclamationFormType extends AbstractType
 {
+    private $formFactory; // Déclarer la propriété formFactory
+
+    public function __construct(FormFactoryInterface $formFactory) // Injecter FormFactoryInterface dans le constructeur
+    {
+        $this->formFactory = $formFactory;
+    }
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        
         $builder
         ->add('satisfait', ChoiceType::class, [
             'choices' => [
@@ -27,16 +41,40 @@ class AvisReclamationFormType extends AbstractType
                 '3' => '3',
                 '4' => '4',
                 '5' => '5',
-                '6' => '6',
-                '7' => '7',
-                '8' => '8',
-                '9' => '9',
-                '10' => '10',
+                
             ],
             'expanded' => true,
             'multiple' => false,
+            'required' => false, // Définir le champ comme non obligatoire
+            'constraints' => [
+                new Callback([
+                    'callback' => function ($value, ExecutionContextInterface $context) {
+                        $form = $context->getRoot();
+                        $data = $form->getData();
+
+                        if ($data['satisfait'] && in_array($value, ['1', '2', '3'])) {
+                            $context->addViolation("Si vous êtes satisfait, veuillez sélectionner une note de 4 ou 5.");
+                        }
+
+                        // Ajouter une nouvelle validation pour empêcher la sélection de notes 4 ou 5 si "satisfait" est défini sur "Non"
+                        if (!$data['satisfait'] && in_array($value, ['4', '5'])) {
+                            $context->addViolation("Si vous êtes non satisfait, veuillez sélectionner une note 1 ou 2 ou 3.");
+                        }
+                    },
+                ]),
+            ],
+            'choice_attr' => function ($choice, $key, $value) use ($builder) {
+                $data = $builder->getData();
+                $satisfait = isset($data['satisfait']) ? $data['satisfait'] : null;//si on clique sur oui ou non il prend cette valeur sinon null
+                if ($satisfait && in_array($choice, ['1', '2', '3'])) {
+                    return ['disabled' => 'disabled'];//  si satsifait on desactive la note 1 2 et 3
+                }
+                return [];
+            },
+            
+            
         ])
-        //->add('commentaire', TextareaType::class)
+
       ;
     }
 
